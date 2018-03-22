228a5a412f9bb8a56eab7e60dab3bc24bee36656
Vegard Veiset <vegard.veiset@sparebank1.no>
2018-03-22 13:02:09 +0100

    Combine validation rules. Create Invalid and Valid status based on validation result.

---
+++ src/main/kotlin/sb1/validation/accountValidator.kt
@@ -0,0 +1,17 @@
+package sb1.validation
+
+internal val accountCannotStartWithZero: Validator<String> = Validator(
+        validator = { account -> account.first() == '0' },
+        reason = "Kan ikke starte med 0")
+
+internal val accountLength: Validator<String> = Validator(
+        validator = { a -> a.length == 11 },
+        reason = "Ikke rett lengde")
+
+internal val accountMustBeNumbers: Validator<String> = Validator({ account -> account.matches("\\d+".toRegex()) })
+
+
+fun accountValidator(account: String) =
+        listOf(accountCannotStartWithZero, accountLength, accountMustBeNumbers)
+                .map { it.validate(account) }
+                .errors()
